bb89503fe657c993e600dafeb3b57a0602d4b309
KrishnaChennuary <95394215+KrishnaChennuary@users.noreply.github.com>
2021-12-05 12:39:58 +1100

    Update block_volume.tf

--- block_volume.tf
+++ block_volume.tf
@@ -3,7 +3,7 @@
 
 resource "oci_core_volume" "postgresql_master_volume" {
   count               = var.add_iscsi_volume ? 1 : 0
-  availability_domain = var.availablity_domain_name
+  availability_domain_name = var.availablity_domain_name
   compartment_id      = var.compartment_ocid
   display_name        = "PostgreSQL_Master_Volume"
   size_in_gbs         = var.iscsi_volume_size_in_gbs
@@ -17,12 +17,12 @@ resource "oci_core_volume_attachment" "postgresql_master_volume_attachment" {
   volume_id       = oci_core_volume.postgresql_master_volume[0].id
 }
 
-resource "oci_core_volume_backup" "postgresql_master_volume_backup" {
-  count        = (var.add_iscsi_volume && var.boot_volume_initial_backup) ? 1 : 0
-  volume_id    = oci_core_volume.postgresql_master_volume[0].id
-  display_name = "PostgreSQL_Master_Volume_Backup_FULL"
-  type         = "FULL"
-}
+# resource "oci_core_volume_backup" "postgresql_master_volume_backup" {
+#   count        = (var.add_iscsi_volume && var.boot_volume_initial_backup) ? 1 : 0
+#   volume_id    = oci_core_volume.postgresql_master_volume[0].id
+#   display_name = "PostgreSQL_Master_Volume_Backup_FULL"
+#   type         = "FULL"
+# }
 
 resource "oci_core_volume_backup_policy_assignment" "postgresql_master_volume_backup_policy_assignment" {
   count     = (var.add_iscsi_volume && var.block_volume_backup_policy_enabled) ? 1 : 0
